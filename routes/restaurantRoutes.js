7d27606af110518841465b70753a4766e21fbd30
Xoli <xolinxiweni@gmail.com>
2024-12-19 13:33:07 +0200

    fixed a server push error

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -384,7 +384,7 @@ const router = express.Router();
 // Configure multer for file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Directory to store uploaded files
+    cb(null, 'uploads/');
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalname); // Unique filename
